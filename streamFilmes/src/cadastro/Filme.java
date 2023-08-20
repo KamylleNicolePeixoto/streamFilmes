53008c9326907c523915b4ee84da6b442ee11054
KamylleNicolePeixoto <kamyllepeixoto@gmail.com>
2023-08-20 19:37:44 -0300

    Criação do projeto

---
+++ streamFilmes/src/cadastro/Filme.java
@@ -0,0 +1,14 @@
+package cadastro;
+
+public class Filme {
+	private String titulo;
+	private int ano;
+	private double avaliacao;
+	
+	public Filme(String titulo, int ano, double avaliacao) {
+		super();
+		this.titulo = titulo;
+		this.ano = ano;
+		this.avaliacao = avaliacao;
+	}
+}
---
+++ streamFilmes/src/cadastro/main.java
@@ -0,0 +1,10 @@
+package cadastro;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste");
+	}
+
+}
